bf311c7b323349548e8a37d4391cb41d93c8de77
ariges <saikrishna.arige@gmail.com>
2023-08-10 18:39:24 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 PORT = 8000
-MESSAGE = "Hello, world! \n This is NCPLACR01 app(v-2.0.11). \n"
+MESSAGE = "Hello, world! \n This is NCPLACR02 app(v-2.0.11). \n"
 
 app = Flask(__name__)
 
